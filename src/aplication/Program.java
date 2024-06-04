2f2db7670c9c58cdcc6a9c97fe2a8a30786bd92a
Mailza <mailza.souzalima2023@gmail.com>
2024-06-03 21:02:42 -0300

    inserindo quebra de linha

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Enter the number of products: ");
-			sc.nextInt();
-		System.out.println("Product #1 data:");	
+		sc.nextInt();
+		System.out.println("Product #1 data:");
 		System.out.print("Common, used or imported (c/u/i)? ");
 		sc.next().charAt(0);
 		System.out.print("Name: ");
@@ -22,14 +22,14 @@ public class Program {
 		sc.nextDouble();
 		System.out.print("Customs fee: ");
 		sc.nextDouble();
-		System.out.println("Product #2 data:");	
+		System.out.println("Product #2 data:");
 		System.out.print("Common, used or imported (c/u/i)? ");
 		sc.next().charAt(0);
 		System.out.print("Name: ");
 		sc.nextLine();
 		System.out.println("Price: ");
 		sc.nextDouble();
-		System.out.println("Product #3 data:");	
+		System.out.println("Product #3 data:");
 		System.out.print("Common, used or imported (c/u/i)? ");
 		sc.next().charAt(0);
 		System.out.print("Name: ");
@@ -38,8 +38,7 @@ public class Program {
 		System.out.print("Price: ");
 		sc.nextDouble();
 		System.out.print("Manufacture date (DD/MM/YYYY): ");
-		
-		
+
 		sc.close();
 	}
 
