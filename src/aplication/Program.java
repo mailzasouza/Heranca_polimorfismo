4b87df17a8027c6c2b3a4ab29b1cb989bdfb4764
Mailza <mailza.souzalima2023@gmail.com>
2024-06-04 10:11:35 -0300

    acrescentando condiçao

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,8 @@
 package aplication;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -13,31 +15,33 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		List <Product> list = new ArrayList<>();
+
+		SimpleDateFormat data = new SimpleDateFormat("dd/MM/yyyy");
+		List<Product> list = new ArrayList<>();
 
 		System.out.print("Enter the number of products: ");
 		int n = sc.nextInt();
-		
-		for(int i = 1; i < n; i++) {
-			System.out.println("Product #" + i + " data:");	
+
+		for (int i = 1; i < n; i++) {
+			System.out.println("Product #" + i + " data:");
 			System.out.print("Common, used or imported (c/u/i)? ");
-			char ch = sc.next().charAt(0);	
+			char ch = sc.next().charAt(0);
 			System.out.print("Name: ");
 			sc.nextLine();
 			String name = sc.nextLine();
 			System.out.print("Price: ");
 			double price = sc.nextDouble();
-			System.out.print("Customs fee: ");
-			double custom = sc.nextDouble();
-			
-			
+			if (ch == 'i') {
+				System.out.print("CustomsFee: ");
+				double customFee = sc.nextDouble();
+				if (ch == 'u') {
+					System.out.println("Manufacture date (DD/MM/YYYY): ");
+					
+
+				}
+			}
+
 		}
-		
-		
-		
-		
-		System.out.print("Manufacture date (DD/MM/YYYY): ");
 
 		sc.close();
 	}
