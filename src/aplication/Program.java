46d56858fdc8d496f2eef124be18439c9b52697f
Mailza <mailza.souzalima2023@gmail.com>
2024-06-04 09:30:00 -0300

    corrigindo classe Program

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,7 +1,11 @@
 package aplication;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Product;
 
 public class Program {
 
@@ -9,34 +13,30 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		List <Product> list = new ArrayList<>();
 
 		System.out.print("Enter the number of products: ");
-		sc.nextInt();
-		System.out.println("Product #1 data:");
-		System.out.print("Common, used or imported (c/u/i)? ");
-		sc.next().charAt(0);
-		System.out.print("Name: ");
-		sc.nextLine();
-		System.out.println();
-		System.out.print("Price: ");
-		sc.nextDouble();
-		System.out.print("Customs fee: ");
-		sc.nextDouble();
-		System.out.println("Product #2 data:");
-		System.out.print("Common, used or imported (c/u/i)? ");
-		sc.next().charAt(0);
-		System.out.print("Name: ");
-		sc.nextLine();
-		System.out.println("Price: ");
-		sc.nextDouble();
-		System.out.println("Product #3 data:");
-		System.out.print("Common, used or imported (c/u/i)? ");
-		sc.next().charAt(0);
-		System.out.print("Name: ");
-		sc.nextLine();
-		System.out.println();
-		System.out.print("Price: ");
-		sc.nextDouble();
+		int n = sc.nextInt();
+		
+		for(int i = 1; i < n; i++) {
+			System.out.println("Product #" + i + " data:");	
+			System.out.print("Common, used or imported (c/u/i)? ");
+			char ch = sc.next().charAt(0);	
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Price: ");
+			double price = sc.nextDouble();
+			System.out.print("Customs fee: ");
+			double custom = sc.nextDouble();
+			
+			
+		}
+		
+		
+		
+		
 		System.out.print("Manufacture date (DD/MM/YYYY): ");
 
 		sc.close();
